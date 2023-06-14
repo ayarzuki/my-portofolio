d005a158a8df8e97aff9fc31e3a65623079079c7
ayarzuki <aulya.yarzuki@gmail.com>
2023-06-14 09:22:20 +0700

    add landing page

---
+++ send_email.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $to = "aulya.yarzuki@gmail.com";
+    $subject = "New message from your website";
+    $body = "Name: " . $name . "\n";
+    $body .= "Email: " . $email . "\n";
+    $body .= "Message: " . $message;
+
+    $headers = "From: " . $email;
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Failed to send email. Please try again later.";
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
